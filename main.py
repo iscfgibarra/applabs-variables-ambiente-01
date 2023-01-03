3ae9c5e28b0eadc336b580dfbd490d8d4beeb767
Francisco Ibarra <silentium77@hotmail.com>
2023-01-03 10:21:00 -0600

    Correciones ortograficas

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from config import config_from_file
 
 
 def main():
-    # Vienen de la configuracion de variables ya establecida en el sistema operativo
+    # Vienen de la configuración de variables ya establecida en el sistema operativo
     print("Variables desde el SO: ")
     print(config_from_os.aws_database)
     print(config_from_os.aws_secret)
 
     print("Variables desde el archivo .env: ")
-    # Vienen de la configuracion "artificial" creada por medio del archivo .env y load_env
+    # Vienen de la configuración "artificial" creada por medio del archivo .env y load_env
     print(config_from_file.aws_database)
     print(config_from_file.aws_secret)
 
